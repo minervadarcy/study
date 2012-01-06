18c3c8be0a13fc40b1c3f6848d2f7b56e31283ce
minervadarcy <minervadarcy@yahoo.com>
2012-01-05 18:09:19 -0800

    add sorting solution

---
+++ sorting/sorting/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sorting
+{
+    public class BubbleSort
+    {
+        //algorithm is O(N^2)
+        public BubbleSort()
+        {
+        }
+
+        public List<int> Sort(List<int> unsortedList)
+        {
+            List<int> sortedList = new List<int>();
+           // write code stuff here.
+            return sortedList;
+        }
+
+    }
+}
---
+++ sorting/sorting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bubble Sort
+            // Quick Sort
+            // Insertion Sort
+            // Selection Sort
+            // Merge Sort
+            // Heap Sort
+            // Shell Sort
+            //http://www.squidoo.com/sorting-algorithms
+
+        }
+    }
+}
