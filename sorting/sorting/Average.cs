187fdddb67c74c64c566f5ceb6f28dfdf789ac51
minervadarcy <minervadarcy@yahoo.com>
2012-01-23 16:59:55 -0800

    add average# code and start the binary sort one

---
+++ sorting/sorting/Average.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sorting
+{
+    class Average
+    {
+        // Get the average # in an array. warmup
+
+        public Average()
+        {
+        }
+
+        public int AverageArray(int[] array)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < array.Count(); i++)
+            {
+                answer = answer + array[i];
+            }
+            answer = answer / array.Count();
+
+            return answer;
+        }
+    }
+}
---
+++ sorting/sorting/BinarySort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sorting
+{
+    class BinarySort
+    {
+
+        //ya i gotta create a search value on a binary search
+    }
+}
